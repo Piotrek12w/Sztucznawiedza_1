63a8246c6dd5cf63c267a31f0fb6c5c6a9d0ebb3
Piotrek12w <129010312+Piotrek12w@users.noreply.github.com>
2023-06-16 21:24:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const square = document.getElementById('square');
+
+square.addEventListener('click', () => {
+    const message = prompt('Napisz coś:');
+
+if (message === 'cześć') {
+    alert('Cześć');
+}else if (message === 'co tam u ciebie') {
+    alert('Dobrze a u ciebie');
+}else if (message === 'wiesz ile to 2+2') {
+    alert('Tak,Jest to cztery');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
